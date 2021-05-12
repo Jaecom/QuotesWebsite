2f232b89159d1899c8338caeebd7be0bf7795c03
Jaecom <50648539+Jaecom@users.noreply.github.com>
2021-05-12 18:55:59 +0900

    Setup with express and ejs

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.set("views", path.join(__dirname, "/views"));
+app.set("view engine", "ejs")
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+})
