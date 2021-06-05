28e903ae52579f2f3c52b8d8fa07ba87a4410279
Jaecom <50648539+Jaecom@users.noreply.github.com>
2021-06-06 00:22:55 +0900

    Throttling scroll events
    Spaces out when scroll events are fired - prevents multiple and sometimes duplicate ajax requests from being called

--- public/javascripts/infiniteScroll.js
+++ public/javascripts/infiniteScroll.js
@@ -10,22 +10,23 @@ let template;
     template = ejs.compile(await fetchTemplateString());
 })();
 
+window.addEventListener("scroll", 
+    throttle(async function () {
+        if (reachBottom()) {
+            if (noFurtherRequest) {
+                return;
+            }
 
-window.onscroll = async function () {
-    if (reachBottom()) {
-
-        if (noFurtherRequest) {
-            return;
+            const quotes = await getQuoteData();
+            createElementAndAppend(quotes);
+            history.pushState(null, '', `?page=${nextPageCount}`);
+            nextPageCount++;
         }
-
-        const quotes = await getQuoteData();
-        createElementAndAppend(quotes);
-        history.pushState(null, '', `?page=${nextPageCount}`);
-        nextPageCount++;
-    }
-}
+    }, 70)
+)
 
 const getQuoteData = async () => {
+
     try {
         const urlWithParams = new URL(baseUrl, host);
         urlWithParams.searchParams.append("page", nextPageCount);
@@ -71,5 +72,18 @@ async function fetchTemplateString() {
 }
 
 const reachBottom = () => {
-    return window.innerHeight + window.scrollY >= document.body.offsetHeight - 150;
+    return window.innerHeight + window.scrollY >= document.body.offsetHeight - 500;
+}
+
+function throttle (callback, limit) {
+    let waiting = false;                      // Initially, we're not waiting
+    return function () {                      // We return a throttled function
+        if (!waiting) {                       // If we're not waiting
+            callback.apply(this, arguments);  // Execute users function
+            waiting = true;                   // Prevent future invocations
+            setTimeout(function () {          // After a period of time
+                waiting = false;              // And allow future invocations
+            }, limit);
+        }
+    }
 }
