20951b18a2c13d95a77dba062027fa504bd3b77a
Jaecom <50648539+Jaecom@users.noreply.github.com>
2021-07-30 21:52:04 +0900

    Development enviroment
    Livereload & express for static files, sass compile on sass file change, along with nodemon

--- app.js
+++ app.js
@@ -4,6 +4,22 @@ const path = require("path");
 const ejsMate = require("ejs-mate");
 const mongoose = require("mongoose");
 
+
+// Develop Enviroment
+const livereload = require("livereload");
+const connectLiveReload = require("connect-livereload");
+
+const liveReloadServer = livereload.createServer();
+liveReloadServer.watch(path.join(__dirname, "/public"));
+app.use(connectLiveReload());
+
+liveReloadServer.server.once("connection", () => {
+    setTimeout(() => {
+        liveReloadServer.refresh("/")
+    }, 100)
+});
+
+
 mongoose.connect("mongodb://localhost:27017/quoteWebsite",
     {
         useNewUrlParser: true,
@@ -21,7 +37,6 @@ app.engine("ejs", ejsMate);
 app.set("views", path.join(__dirname, "/views"));
 app.set("view engine", "ejs");
 app.use(express.static('public'));
-
 
 const quotesRouter = require("./routes/quotes");
 const apiRouter = require("./routes/api")
